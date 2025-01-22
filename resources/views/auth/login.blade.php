fef4190884c1a5e80a6eac7edbb468b263449a11
natxoz <nacho.llombart@biocomenergia.com>
2025-01-22 16:20:15 +0100

    mejoras login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,11 @@
 <x-guest-layout>
-  <x-authentication-card>
+
+  <x-authentication-card class="">
+    <!-- Agregado padding y margen automático para centrar -->
     <x-slot name="logo">
-      <!-- <x-authentication-card-logo /> -->
-      <img src="{{ asset('images/logo-selev-biogroup.png') }}" width="100">
+      <img src="{{ asset('images/logo-selev-biogroup.png') }}" width="250">
     </x-slot>
 
-
-
-
-    <style>
-      /* Estilos básicos para simular una app móvil */
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: Arial, sans-serif;
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-        background-color: #f7f7f7;
-      }
-
-      .navbar {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        padding: 10px 20px;
-        background-color: #79B329;
-        color: white;
-      }
-
-      .navbar .left-section {
-        display: flex;
-        align-items: center;
-      }
-
-      .navbar .left-section h1 {
-        margin: 0;
-        font-size: 18px;
-      }
-
-      .navbar .right-section {
-        display: flex;
-        gap: 10px;
-      }
-
-      .navbar button {
-        background-color: #5C6562;
-        color: white;
-        border: none;
-        padding: 5px 10px;
-        cursor: pointer;
-        border-radius: 10px;
-        /* Valor alto para bordes redondeados */
-      }
-
-      .navbar a {
-        background-color: #5C6562;
-        color: white;
-        border: none;
-        padding: 5px 10px;
-        cursor: pointer;
-        border-radius: 10px;
-        /* Valor alto para bordes redondeados */
-      }
-
-
-      .content {
-        flex-grow: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-      }
-    </style>
 
     <x-validation-errors class="mb-4" />
 
@@ -83,22 +15,24 @@
     </div>
     @endif
 
+
     <form method="POST" action="{{ route('login') }}">
       @csrf
 
       <div>
-        <x-label for="username" value="{{ __('Username') }}" />
+        <x-label for="username" value="NIF" />
         <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
       </div>
 
       <div class="mt-4">
-        <x-label for="password" value="{{ __('Password') }}" />
+        <x-label for="password" value="Contraseña" />
         <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
       </div>
 
-      <div class="flex items-center justify-end mt-4">
-        <x-button class="ms-4">INCIAR SESION</x-button>
+      <div class="flex items-center justify-center mt-4">
+        <x-button class="ms-4">ENTRAR</x-button>
       </div>
     </form>
   </x-authentication-card>
+
 </x-guest-layout>
